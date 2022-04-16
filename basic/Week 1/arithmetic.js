3b3478523ca80208fcbc0eccabe00003492d55da
Spencer Smith <smith,spence13@gmail.com>
2022-04-15 21:00:40 -0700

    started algorith folder/basic arithmetic Javascript

---
+++ basic/Week 1/arithmetic.js
@@ -0,0 +1,17 @@
+// Fill in the body of each function with the code required
+
+const add = function(num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
